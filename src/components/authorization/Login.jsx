d04781c65e0e6502c056d2011ee6ecc98c2b43ab
Zhannur <jannuraidynuly@gmail.com>
2022-01-18 13:21:32 +0500

    add component Login

--- src/components/authorization/Login.jsx
+++ src/components/authorization/Login.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import "./login.css";
 
 const Login = () => {
     return (
-        <section>
-            <div className="color"></div>
-            <div className="color"></div>
-            <div className="color"></div>
-        </section>
+        <div className="box">
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="container">
+                <div className="form">
+                    <h2>Login Form</h2>
+                    <form>
+                        <div className="inputBox">
+                            <input type="text" placeholder="Username" />
+                        </div>
+                        <div className="inputBox">
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <div className="inputBox">
+                            <input type="submit" value="Login" />
+                        </div>
+                        <p className="forget">Forgot Password ? <a href="#">Click Here</a></p>
+                        <p className="forget">Don't have an account ? <a href="#">Sign up</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 };
 
