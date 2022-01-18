05749f067cfb4896c34fbe5dbb944cf0e496da3d
Zhannur <jannuraidynuly@gmail.com>
2022-01-18 22:24:49 +0500

    create login connect

--- src/actions/user.js
+++ src/actions/user.js
@@ -15,3 +15,16 @@ export const registration = async (firstName, lastName, email, password) => {
         alert(e.response.data.message)
     }
 }
+
+export const login = async (email, password) => {
+    try {
+        const response = await axios.post(`${API_URL}api/auth/login`, {
+            email,
+            password
+        })
+        localStorage.setItem('tokenchik', response.data.token)
+        console.log(response.data)
+    } catch(e) {
+        alert(e.response.data.message)
+    }
+}
--- src/components/authorization/Login.jsx
+++ src/components/authorization/Login.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
+import { login } from "../../actions/user";
 import "./authorization.css";
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <div className="box">
             
@@ -14,18 +19,18 @@ const Login = () => {
                     <div className="square"></div>
                 <div className="form">
                     <h2>Войдите в свою учетную запись</h2>
-                    <form>
+                    <div>
                         <div className="inputBox">
-                            <input type="text" placeholder="Адрес электронной почты" />
+                            <input type="text" placeholder="Адрес электронной почты" value={email} onChange={(event) => setEmail(event.target.value)}/>
                         </div>
                         <div className="inputBox">
-                            <input type="password" placeholder="Пароль" />
+                            <input type="password" placeholder="Пароль" value={password} onChange={(event) => setPassword(event.target.value)}/>
                         </div>
                         <div className="inputBox">
-                            <input type="submit" value="Войти" />
+                            <input type="submit" value="Войти" onClick={() => login(email, password)}/>
                         </div>
                         <p className="forget">У вас нет аккаунта ? <NavLink to="/signUp">Зарегистрироваться</NavLink></p>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
