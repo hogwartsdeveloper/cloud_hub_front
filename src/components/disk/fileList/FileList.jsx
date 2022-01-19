eeb46a266408ee684c3607ce2ecf608e8398e752
Zhannur <jannuraidynuly@gmail.com>
2022-01-19 19:12:58 +0500

    file reducer

--- src/components/disk/fileList/FileList.jsx
+++ src/components/disk/fileList/FileList.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./fileList.less";
 
 const FileList = () => {
+    const files = useSelector(state => state.files.files)
+
+    if (files.length === 0) {
+        return (
+            <div className="loader">Файлы не найдены</div>
+        )
+    }
     return (
         <div className="fileList">
             <div className="fileList__header">
