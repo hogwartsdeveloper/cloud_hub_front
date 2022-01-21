504daff22029957b162b17de24d87b9bf59c0c10
Zhannur <jannuraidynuly@gmail.com>
2022-01-21 11:01:26 +0500

    navbar link profile and avatar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,13 +6,17 @@ import { getFiles, searchFile } from "../../actions/file";
 import { logout } from "../../reducers/userReducer";
 import Input from "../UI/input/Input";
 import './navbar.css';
+import avatarLogo from "../../asserts/img/avatar.svg"
+import { API_URL } from "../../config";
 
 const Navbar = () => {
     const isAuth = useSelector(state => state.user.isAuth)
     const currentDir = useSelector(state => state.files.currentDir)
+    const currentUser = useSelector(state => state.user.currentUser)
     const [searchName, setSearchName] = useState('')
     const [searchTimeout, setSearchTimout] = useState(false)
     const dispatch = useDispatch()
+    const avatar = currentUser.avatar ? `${API_URL + currentUser.avatar}` : avatarLogo
 
     function searchChangeHandler(value) {
         setSearchName(value)
@@ -36,6 +40,9 @@ const Navbar = () => {
             <ul>
                 {!isAuth && <li><NavLink to="/signIn">Войти</NavLink></li>}
                 {!isAuth && <li><NavLink to="/signUp">Попробовать бесплатно</NavLink></li>}
+                {isAuth && <li><NavLink to="/profile" className="avatar__link">
+                    <img className="navbar__avatar" src={avatar} alt="avatar" />    
+                </NavLink></li>}
                 {isAuth && <li><NavLink to="/" onClick={() => dispatch(logout())}>Выход</NavLink></li>}
             </ul>
         </div>
